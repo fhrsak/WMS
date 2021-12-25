2649b17ccde7e61faaeb6265f5bb7e3854de8650
Filip Hršak <fhrsak@racunarstvo.hr>
2021-12-26 00:34:05 +0100

    Initial commit

---
+++ src/main/java/hr/hrsak/wms/WmsApplication.java
@@ -0,0 +1,13 @@
+package hr.hrsak.wms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsApplication.class, args);
+    }
+
+}
